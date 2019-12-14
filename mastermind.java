53dcb8e3ce3176c32460bee507ef7ff8d10e0075
Archie <archie@Archibalds-Air.home>
2019-12-14 20:26:20 +0000

    nearly done

--- mastermind.java
+++ mastermind.java
@@ -2,33 +2,32 @@ import java.util.*;
 import java.util.stream.Stream;
 import java.util.stream.Collectors;
 
-
-public class mastermind
+class player
 {
-    public class player
+    public String name;
+    public player(String name_in)
     {
-        public String name;
-        public player(String name_in)
-        {
-            name = name_in;
-        }
+        name = name_in;
     }
+}
 
-    public class feedback
+class feedback
+{
+    public int white_hits, black_hits;
+    public feedback(int wh_in, int bh_in)
     {
-        public int white_hits, black_hits;
-        public feedback(int wh_in, int bh_in)
-        {
-            white_hits = wh_in;
-            black_hits = bh_in;
-        }
-        public boolean equals(feedback other)
-        {
-            return ((white_hits == other.white_hits) && (black_hits == other.black_hits));
-        }
+        white_hits = wh_in;
+        black_hits = bh_in;
     }
-
-    public feedback compare_codes(String code1, String code2)
+    public boolean equals(feedback other)
+    {
+        return ((this.white_hits == other.white_hits) && (this.black_hits == other.black_hits));
+    }
+    public String toString()
+    {
+        return white_hits + ", " + black_hits;
+    }
+    public static feedback compare_codes(String code1, String code2)
     {
         int black_hits = 0;
         int white_hits = 0;
@@ -51,48 +50,58 @@ public class mastermind
         }
         return new feedback(white_hits, black_hits);
     }
+}
 
-    public class master extends player
+
+
+class master extends player
+{
+    private String code;   
+    public master(String name_in)
     {
-        private String code;   
-        public master(String name_in)
+        super(name_in);
+        Random r = new Random();
+        code = "";
+        for (int i = 0 ; i < 4; i++)
         {
-            super(name_in);
-            Random r = new Random();
-            code = "";
-            for (int i = 0 ; i < 4; i++)
-            {
-                code += (r.nextInt(6)+1);
-            }
-        }
-        public feedback evaluate_guess(String guess)
-        {
-            return compare_codes(guess, code);
+            code += (r.nextInt(6)+1);
         }
     }
-
-    public class mind extends player
+    public feedback evaluate_guess(String guess)
     {
-        public Set<String> guess_set;
-        public mind(String name_in) 
-        {
-            super(name_in);
-            guess_set = generate_combinations("",4);
-        }
+        return feedback.compare_codes(guess, code);
+    }
+}
 
-        public String make_guess()
-        {
-            return guess_set.iterator().next();
-        }
-        public void learn(feedback f)
-        {
-            // guess_set = new HashSet<String>();
-            // guess_set = guess_set.stream().filter(x -> compare_codes(x,guess_set.iterator().next()) == f).collect(Collectors.toSet());
-            guess_set.removeIf(x -> compare_codes(x,guess_set.iterator().next()) == f);
-        }
+class mind extends player
+{
+    public Set<String> guess_set;
+    public mind(String name_in) 
+    {
+        super(name_in);
+        guess_set = generate_combinations("",4);
     }
 
-    public static Set<String> generate_combinations(String input, int depth)
+    public String make_guess()
+    {
+        return guess_set.iterator().next();
+    }
+    public void learn(feedback f)
+    {
+        // guess_set = new HashSet<String>();
+        // guess_set = guess_set.stream().filter(x -> compare_codes(x,guess_set.iterator().next()) == f).collect(Collectors.toSet());
+        String last_guess = guess_set.iterator().next();
+        guess_set.removeIf(x -> !feedback.compare_codes(x, last_guess).equals(f));
+        // System.out.println("code " + last_guess + " produced feedback: " + f);
+        // for (var x : guess_set)
+        // {
+        //     var n = feedback.compare_codes(x, last_guess);
+        //     System.out.println(n + ", match? " + (f.equals(n)));
+            
+        // }
+    }
+
+    private static Set<String> generate_combinations(String input, int depth)
     {
         var set = new HashSet<String>();
         if (depth != input.length())
@@ -108,24 +117,36 @@ public class mastermind
         }
         return set;
     }
+}
 
-
-
+public class mastermind
+{
     public static void main(String[] args)
     {
-        master ma = master("Archie");
-        // mind mi = new mind("George");
-        // Set a = new HashSet<int>();
-        var set = new HashSet<String>();
-
-        // mi.learn(ma.evaluate_guess(mi.make_guess()));
-        // System.out.println(mi.guess_set.size());
+        master ma = new master("Archie");
+        mind mi = new mind("George");
+        // System.out.println(mi.make_guess());
+        // System.out.println(ma.evaluate_guess(mi.make_guess()));
+        feedback ideal = new feedback(0,4);
+        // feedback result = new feedback();
+        for(;;)
+        {
+            mi.learn(ma.evaluate_guess(mi.make_guess()));
+            System.out.println(mi.guess_set.size());
+        }
 
         // var c = generate_combinations("", 6);
         // System.out.println(c);
         // System.out.println(c.size());
         // System.out.println(c.iterator().next());
 
-    }
+        // var s = new HashSet<String>();
+        // for (int i = 0; i < 11;i++)
+        // {
+        //     s.add("" + i);
+        // }
+        // s.removeIf(i -> (Integer.parseInt(i) % 2) == 0);
+        // System.out.println(s);
 
+    }
 }
